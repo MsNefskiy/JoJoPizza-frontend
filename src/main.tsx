a0d5b3c5a67dfc1758d3ca028990a80b66223ad0
MsNefskiy <salionk223457162@gmail.com>
2023-03-17 17:50:11 +0300

    feature: add redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,29 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<App/>}>
-            <Route index={true} element={<Home/>} />
-            <Route path="cart" element={<Cart/>} />
-            <Route path="*" element={<NotFound/>}/>
-        </Route>
-    )
-)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<Home />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
