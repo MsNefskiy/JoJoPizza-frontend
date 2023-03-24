27fde47779b1a2473e5c7120d778d53ffa380105
MsNefskiy <salionk223457162@gmail.com>
2023-03-24 17:36:25 +0300

    style: add temporary styles for cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
-import "./scss/app.scss";
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import './scss/app.scss';
 
 function App() {
   return (
-    <>
-      <div className="wrapper">
-        <Header />
-        <Outlet/>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Outlet />
       </div>
-    </>
+    </div>
   );
 }
 
