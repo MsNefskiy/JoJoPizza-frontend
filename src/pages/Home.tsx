50ea5233b1fd53200e1ba9b3bf7518a8001d6f92
MsNefskiy <salionk223457162@gmail.com>
2023-03-24 18:56:26 +0300

    feature: handle the error status when the menu is loaded unsuccessfully

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,32 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from 'react';
 
-import Categories from "../components/Categories";
-import ProductBlock from "../components/ProductBlock";
-import Sort from "../components/Sort";
-import "../scss/app.scss";
-
-interface IProduct {
-  title: string;
-  image: string;
-  price: number;
-  rating: number;
-  specifications: {
-    types: number[];
-    sizes: number[];
-    category: number;
-  };
-  _id: string;
-}
+import Categories from '../components/Categories';
+import ProductBlock from '../components/ProductBlock';
+import Sort from '../components/Sort';
+import '../scss/app.scss';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { fetchProducts } from '../redux/product/asyncActions';
+import { Status } from '../redux/product/types';
 
 function Home() {
-  const [products, setProducts] = useState<IProduct[]>([]);
+  const { items, status } = useAppSelector((state) => state.product);
+  const dispacth = useAppDispatch();
 
-  async function getMenu() {
-    await axios
-      .get<IProduct[]>("http://localhost:3000/api/product")
-      .then((response) => {
-        setProducts(response.data);
-      });
-  }
+  const products = items.map((product) => (
+    <ProductBlock
+      key={product['_id']}
+      id={product['_id']}
+      title={product.title}
+      image={product.image}
+      types={product.specifications.types}
+      sizes={product.specifications.sizes}
+      price={product.price}
+    />
+  ));
+
+  const getMenu = () => {
+    dispacth(fetchProducts());
+  };
 
   useEffect(() => {
     getMenu();
@@ -36,26 +34,19 @@ function Home() {
 
   return (
     <>
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <div className="content__title">Все пиццы</div>
-          <div className="content__items">
-            {products.map((product) => (
-              <ProductBlock
-                key={product["_id"]}
-                title={product.title}
-                image={product.image}
-                types={product.specifications.types}
-                sizes={product.specifications.sizes}
-                price={product.price}
-              />
-            ))}
-          </div>
+      <div className="container">
+        <div className="content__top">
+          <Categories />
+          <Sort />
         </div>
+        <div className="content__title">Все пиццы</div>
+        {status === Status.ERROR ? (
+          <div>Ошибочка вышла...</div>
+        ) : (
+          <div className="content__items">
+            {status === Status.LOADING ? <p>Идёт загрузка...</p> : products}
+          </div>
+        )}
       </div>
     </>
   );
