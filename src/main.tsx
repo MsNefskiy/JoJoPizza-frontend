88bf2ceabc65d558eacc55e0865c8040080b82b1
MsNefskiy <salionk223457162@gmail.com>
2023-03-24 19:05:08 +0300

    feature: add a component FullProduct to display the product on a separate page

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,25 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
+import App from "./App";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import FullProduct from "./components/FullProduct";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<Home />} />
       <Route path="cart" element={<Cart />} />
+      <Route path="/product/:id" element={<FullProduct />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
